b194e995293d178b56483d44a8e625a8f5cfa0b2
Jan Dennis Zapatos <134060585+Dennis-Zapatos@users.noreply.github.com>
2024-06-17 18:21:20 +0800

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -44,6 +44,6 @@ def predict_price(item:HouseData):
 
 # Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
-if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+# if __name__ == '__main__':
+#     uvicorn.run(app, host='127.0.0.1', port=8000)
 #uvicorn main:app --reload
