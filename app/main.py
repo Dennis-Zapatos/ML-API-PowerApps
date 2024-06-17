bf625b809bb4e94fe95f0320283a82d47c492e65
Jan Dennis Zapatos <134060585+Dennis-Zapatos@users.noreply.github.com>
2024-06-17 18:53:41 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -47,5 +47,5 @@ def predict_price(item:HouseData):
 # if __name__ == '__main__':
 #     uvicorn.run(app, host='127.0.0.1', port=8000)
 
-uvicorn.run(app)
+uvicorn.run(app, host='0.0.0.0', port=8000)
 #uvicorn main:app --reload
