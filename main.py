80754961f1bbee6e5d77eeac045e9d55cbf5c410
Jan Dennis Zapatos <134060585+Dennis-Zapatos@users.noreply.github.com>
2024-06-24 00:29:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -42,8 +42,8 @@ def predict_price(item:HouseData):
     prediction = pipe.predict(df)
     return {"Price Prediction: ": float(prediction)}
 
-Run the API with uvicorn
-   Will run on http://127.0.0.1:8000
+# Run the API with uvicorn
+#    Will run on http://127.0.0.1:8000
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
 
